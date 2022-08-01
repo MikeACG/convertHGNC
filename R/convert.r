9d66d3133548431ffaf16549208bae0b9795e4bc
Miguel Cortes <a01270966@exatec.tec.mx>
2022-08-01 18:39:48 +0200

    initial commit

---
+++ R/convert.r
@@ -0,0 +1,32 @@
+#' @export
+id2id <- function(ids, queryCol, subjectCol) {
+
+    query <- as.character(hgnc[[queryCol]])
+    subject <- as.character(hgnc[[subjectCol]])
+
+    out <- subject[match(ids, query)]
+
+    return(setNames(out, ids)) 
+
+}
+
+#' @export
+ensembl2entrez <- function(ids) {
+
+    queryCol <- match("ensembl_gene_id", names(hgnc))
+    subjectCol <- match("entrez_id", names(hgnc))
+
+    return(id2id(ids, queryCol, subjectCol))
+
+}
+
+#' @export
+entrez2ensembl <- function(ids) {
+
+    ids <- as.character(ids)
+    queryCol <- match("entrez_id", names(hgnc))
+    subjectCol <- match("ensembl_gene_id", names(hgnc))
+
+    return(id2id(ids, queryCol, subjectCol))
+
+}
